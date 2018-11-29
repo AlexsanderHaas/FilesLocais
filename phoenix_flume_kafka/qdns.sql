8b2cdc3ab556621a46a68c07adf2713fd7704933
Alexsander Haas <alexsander.haas@hotmail.com>
2018-11-29 14:06:07 -0200

    new table

---
+++ phoenix_flume_kafka/qdns.sql
@@ -0,0 +1,22 @@
+--SELECT COUNT (*)
+SELECT 
+--select 
+--tipo,
+ts_code,
+ts,       
+uid,
+id_orig_h,
+--id_orig_p, 
+id_resp_h, 
+--id_resp_p, 
+proto,     
+query,
+answers
+FROM JSON9
+--WHERE tipo = 'DNS';
+WHERE tipo = 'DNS'
+--WHERE tipo = 'CONN'
+--AND TS_CODE >= TO_TIMESTAMP ('2018-11-28 15:09:00.000')
+--AND proto = 'tcp'
+ORDER BY ts;
+--WHERE TS_CODE >= TO_TIMESTAMP ('1542571843'); --https://phoenix.apache.org/language/functions.html#to_timestamp

c83e84d5032c409157759a5e1eac74add4b05196
Alexsander Haas <alexsander.haas@hotmail.com>
2018-12-09 17:48:12 -0200

    Tabela do Kmeans

---
+++ phoenix_flume_kafka/table_kmeans.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS LOG_KMEANS_DDOS (  
+  PROTO         VARCHAR,
+  SERVICE       VARCHAR,
+  ID_ORIG_H     VARCHAR,
+  TS_FILTRO     TIMESTAMP     NOT NULL, --Filtro do TIMESTAMP utilizado na seleção dos dados para análise
+  TS_CODE	      TIMESTAMP     NOT NULL, 
+  ROW_ID        UNSIGNED_LONG NOT NULL, 
+
+  TS            TIMESTAMP, 
+  COUNT         UNSIGNED_LONG,   
+  ID_RESP_H     VARCHAR,     
+  ID_RESP_P     UNSIGNED_LONG, 
+  DURATION      UNSIGNED_LONG,
+  ORIG_PKTS     UNSIGNED_LONG,
+  ORIG_IP_BYTES UNSIGNED_LONG,
+  ORIG_BYTES    UNSIGNED_LONG,
+  RESP_PKTS     UNSIGNED_LONG,
+  RESP_IP_BYTES UNSIGNED_LONG,
+  RESP_BYTES    UNSIGNED_LONG,
+--  FEATURES      DOUBLE ARRAY[1], são as colunas que escolhi para calcular os centroides
+  PREDICTION    INTEGER,
+  CENTROID      VARCHAR[]
+	  
+  CONSTRAINT PK PRIMARY KEY( PROTO     ,
+                             SERVICE   ,
+                             ID_ORIG_H ,
+                             TS_FILTRO ,
+                             TS_CODE   ,
+                             ROW_ID ));
